9187ee59a69498e1fa765a8375876d8f1c4002e4
Andrew Kemp <andrew@kemponline.co.uk>
2023-10-18 14:39:49 +0100

    new script

---
+++ AddNewDomain.ps1
@@ -0,0 +1,9 @@
+$PrimaryDomain = Read-Host "What is the Primary Domain you want to update?"
+$SecondaryDomain = Read-Host "Whatt is the new doomain you want to add as an alias?"
+
+$users = Get-Mailbox | Where-Object{$_.PrimarySMTPAddress -match $PrimaryDomain}
+ 
+foreach($user in $users){
+    Write-Host "Adding Alias $($user.alias)@+$SecondayDomain to $user.PrimarySmtpAddress"
+    #Set-Mailbox $user.PrimarySmtpAddress -EmailAddresses @{add="$($user.Alias)@+$SecondayDomain"}
+}
