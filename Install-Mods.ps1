87c7c0e630ca2c9dc7d8389f011484841cda6d21
Andrew Kemp <andrew@kemponline.co.uk>
2023-10-26 11:28:03 +0100

    new scripts

---
+++ Install-Mods.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy RemoteSigned
+Install-Package NUGet -Force
+Install-Module MSOnline -Force
+Install-Module ExchangeOnlineManagement -Force
+Install-Module Microsoft.Graph -Force
+Install-Module AzureAD -Force
---
+++ Registry Block.ps1
@@ -0,0 +1,9 @@
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings]
+"ProxyEnable"=dword:00000001
+"ProxyServer"="127.0.0.2:8080"
+"ProxyOverride"="*.azure.com;*.azure.net;*.microsoft.com;*.windowsupdate.com;*.microsoftonline.com;*.microsoftonline.cn;*.windows.net;*.windowsazure.com;*.windowsazure.cn;*.azure.cn;*.loganalytics.io;*.applicationinsights.io;*.vsassets.io;*.azure-automation.net;*.visualstudio.com,portal.office.com;*.aspnetcdn.com;*.sharepointonline.com;*.msecnd.net;*.msocdn.com;*.webtrends.com"
+"AutoDetect"=dword:00000000
+
+
+
+*.azure.com;*.azure.net;*.microsoft.com;*.windowsupdate.com;*.microsoftonline.com;*.microsoftonline.cn;*.windows.net;*.windowsazure.com;*.windowsazure.cn;*.azure.cn;*.loganalytics.io;*.applicationinsights.io;*.vsassets.io;*.azure-automation.net;*.visualstudio.com,portal.office.com;*.aspnetcdn.com;*.sharepointonline.com;*.msecnd.net;*.msocdn.com;*.webtrends.com
