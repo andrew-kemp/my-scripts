cf1834c7c0b8310dbd6673249ba38b6ec87632d3
Andrew Kemp <andrew@kemponline.co.uk>
2023-10-26 16:26:53 +0100

    NEw attributes

--- Groupmembers MS Grpah.ps1
+++ Groupmembers MS Grpah.ps1
@@ -1,5 +1,3 @@
-Import-Module Microsoft.Graph.Identity.DirectoryManagement
-Select-MgProfile -Name "beta"
 Connect-mgGraph -Scopes Device.Read.All, Directory.ReadWrite.All, Directory.AccessAsUser.All
 $GroupID = "2973260c-6b0d-4bf9-bf51-276e0e178fce"
 $Devices = Get-MgGroupMember -GroupId $groupId
@@ -7,12 +5,13 @@ $Devices = Get-MgGroupMember -GroupId $groupId
 ForEach ($device in $Devices) {
 
 $uri = $null
-$uri = "https://graph.microsoft.com/beta/devices/" + $device.id
+$uri = "https://graph.microsoft.com/v1.0/devices/" + $device.id
 
 $json = @{
       "extensionAttributes" = @{
       "extensionAttribute1" = "IoT"
       "extensionAttribute2" = "Win365Access"
+      "extensionAttribute3" = "IoT Access Only"
          }
   } | ConvertTo-Json
   
