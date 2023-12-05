61d0eb85df681177c22a938b6443c52731b2419a
Andrew Kemp <andrew@kemponline.co.uk>
2023-12-05 16:35:43 +0000

    Exchange Update

---
+++ ExchangeStuff/Add-ProxyAddresses.ps1
@@ -0,0 +1,7 @@
+Import-Csv “.\EspionUsers_Import.csv” | ForEach-Object{
+    New-MailUser -Name $_.Name -DisplayName $_.DisplayName -SamAccountName $_.SamAccountName -UserPrincipalName $_.BSIEmail -Alias $_.Alias -PrimarySMTPAddress $_.BSIEmail -ExternalEmailAddress $_.PrimarySmtpAddress -OrganizationalUnit "bsi-global.net//Test/Contacts"  -Password (ConvertTo-SecureString -String 'P@ssw0rd1' -AsPlainText -Force)
+      $name = $_.BSIEmail
+      $proxy = $_.emailaddresses -split ' '
+      Set-MailUser -Identity $name -EmailAddresses @{add= $proxy}
+    }
+    
